3b560bdb628a64c09c5d3f6d82e8aa2bfe0a6afc
Rinaldo <festarinaldo@gmail.com>
2024-05-03 17:07:38 +0200

    first commit

--- code_reader.py
+++ code_reader.py
@@ -10,11 +10,11 @@ def code_reader_func(file_name):
             return {"file_content": content}
     except Exception as e:
         return {"error": str(e)}
-    
+
+
 code_reader = FunctionTool.from_defaults(
     fn=code_reader_func,
     name="code_reader",
-    description="""This tool can read the contents of code files and return their results. Use this when you need to read the contents of a file""",
+    description="""this tool can read the contents of code files and return 
+    their results. Use this when you need to read the contents of a file""",
 )
-
-
